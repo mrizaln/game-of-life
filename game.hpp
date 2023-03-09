e671710942b873f4ed501a72d0a0202918d461d4
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-03-09 18:41:39 +0700

    Add threading capability
    At this point, this threading capability didn't gave any improvements.
    In spite of that though, I want to to try threading, so I add this
    capability regardless.

--- game.hpp
+++ game.hpp
@@ -1,47 +1,54 @@
-#include <iostream>
-#include <vector>
-#include <random>
+#include <algorithm>
 #include <ctime>
-#include <utility>      // for std::pair
+#include <functional>
+#include <iostream>
+#include <random>
+#include <stdexcept>
+#include <thread>
+#include <utility>    // for std::pair
+#include <vector>
+#include <execution>
 
 #ifndef GAME_H
 #define GAME_H
-
 
 class Cell
 {
 public:
     inline static const bool s_dead_state = false;
 
-
 private:
     bool m_state{};
     bool m_state_next{};
-
 
 public:
     Cell() = default;
 
     constexpr auto isLive() const { return m_state; }
     constexpr auto isDead() const { return !m_state; }
-    constexpr auto setLive() { m_state_next = true; }       // won't be affected before update() has been called
-    constexpr auto setDead() { m_state_next = false; }      // won't be affected before update() has been called
-    
-    void update() { m_state = m_state_next; }
-};
+    constexpr void setLive() { m_state_next = true; }     // won't be affected before update() has been called
+    constexpr void setDead() { m_state_next = false; }    // won't be affected before update() has been called
 
+    void update() { m_state = m_state_next; }
+
+    operator bool() const { return m_state; }
+};
 
 class Grid
 {
 public:
-    using Coord_type = int;
-    using Grid_type = std::vector<std::vector<Cell>>;       // X number of Cells inside Y number of vectors
-
+    using Coord_type     = int;
+    using Grid_type      = std::vector<std::vector<Cell>>;    // X number of Cells inside Y number of vectors
+    using Grid_flat_type = std::vector<Cell>;
 
 private:
-    Coord_type m_width{};
-    Coord_type m_length{};
-    Grid_type m_grid{};
+    Coord_type m_width{};     // Y
+    Coord_type m_length{};    // X
+    Grid_type  m_grid{};      // states
+
+    // threading
+    static constexpr int     MAX_NUM_OF_THREAD{ 64 };
+    std::vector<std::thread> threadPool{};
 
     static int getRandomNumber(int min, int max)
     {
@@ -52,11 +59,10 @@ private:
     // 0 < probability < 1
     static bool getRandomBool(float probability)
     {
-        int num{ getRandomNumber(0,std::numeric_limits<int>::max()) };
+        int   num{ getRandomNumber(0, std::numeric_limits<int>::max()) };
         float numNormalized{ static_cast<float>(num) / std::numeric_limits<int>::max() };
         return numNormalized < probability;
     }
-
 
 public:
     Grid(const Coord_type length, const Coord_type width)
@@ -70,47 +76,99 @@ public:
 
     void populate(const float density)
     {
-        for (auto& row : m_grid)
-        {
-            for (auto& cell : row)
-            {
-                switch (getRandomBool(density))
-                {
-                case true:  cell.setLive(); break;
-                case false: cell.setDead(); break;
+        for (auto& row : m_grid) {
+            for (auto& cell : row) {
+                switch (getRandomBool(density)) {
+                case true:
+                    cell.setLive();
+                    break;
+                case false:
+                    cell.setDead();
+                    break;
                 }
             }
         }
 
-        for (auto& row : m_grid)
-            for (auto& cell : row)
+        for (auto& row : m_grid) {
+            for (auto& cell : row) {
                 cell.update();
+            }
+        }
     }
 
     Grid& updateState()
     {
-        for (Coord_type i{ 0 }; i < m_length; ++i)
-        {
-            for (Coord_type j{ 0 }; j < m_width; ++j)
-            {
-                auto& cell{ (*this)(i, j) };
-                
-                auto numberOfNeighbor{ checkNeighbors(i, j) };
-                
-                if (cell.isLive())
-                {
-                    if      (numberOfNeighbor <  2) cell.setDead();
-                    else if (numberOfNeighbor <= 3) cell.setLive();
-                    else                            cell.setDead();
+        for (Coord_type i{ 0 }; i < m_length; ++i) {
+            if (threadPool.size() >= MAX_NUM_OF_THREAD) {
+                for (auto& thread : threadPool) {
+                    if (!thread.joinable()) {
+                        std::cout << "stuck\n";
+                    }
+                    thread.join();
                 }
-                else
-                    if      (numberOfNeighbor == 3) cell.setLive();
+                threadPool.clear();
             }
+
+            auto i_cur{ i };
+            threadPool.emplace_back([&](int i_cur) {
+                for (Coord_type j{ 0 }; j < m_width; ++j) {
+                    auto& cell{ (*this)(i_cur, j) };
+
+                    auto numberOfNeighbor{ checkNeighbors(i_cur, j) };
+
+                    if (cell.isLive()) {
+                        if (numberOfNeighbor < 2) {
+                            cell.setDead();
+                        } else if (numberOfNeighbor <= 3) {
+                            cell.setLive();
+                        } else {
+                            cell.setDead();
+                        }
+                    } else if (numberOfNeighbor == 3) {
+                        cell.setLive();
+                    }
+                }
+            },
+                i);
         }
 
-        for (auto& row : m_grid)
-            for (auto& cell : row)
-                cell.update();
+        for (auto& thread : threadPool) {
+            thread.join();
+        }
+        threadPool.clear();
+
+        // for (auto& row : m_grid) {
+        //     for (auto& cell : row) {
+        //         cell.update();
+        //     }
+        // }
+
+        std::for_each(std::execution::par_unseq, m_grid.begin(), m_grid.end(), [&](std::vector<Cell>& row) {
+            std::for_each(std::execution::par_unseq, row.begin(), row.end(), [&](Cell& cell) { cell.update(); });
+        });
+
+        // for (Coord_type i{ 0 }; i < m_length; ++i) {
+        //     if (threadPool.size() >= MAX_NUM_OF_THREAD*2) {
+        //         for (auto& thread : threadPool) {
+        //             if (!thread.joinable())
+        //                 std::cout << "stuck\n";
+        //             thread.join();
+        //         }
+        //         threadPool.clear();
+        //     }
+
+        //     auto i_cur{ i };
+        //     threadPool.emplace_back([&](int i_cur) {
+        //         for (Coord_type j{ 0 }; j < m_width; ++j) {
+        //             (*this)(i_cur, j).update();
+        //         }
+        //     }, i);
+        // }
+
+        // for (auto& thread : threadPool) {
+        //     thread.join();
+        // }
+        // threadPool.clear();
 
         return *this;
     }
@@ -122,7 +180,7 @@ public:
     }
 
     // return the number of live neighbors
-    int checkNeighbors(const Coord_type xPos, const Coord_type yPos)
+    constexpr int checkNeighbors(const Coord_type xPos, const Coord_type yPos)
     {
         auto& x{ xPos };
         auto& y{ yPos };
@@ -132,27 +190,35 @@ public:
          *      [x-1, y-1] [x, y-1] [x+1, y-1]
          *      [x-1, y  ] [x, y  ] [x+1, y  ]
          *      [x-1, y+1] [x, y+1] [x+1, y+1]
-        */
+         */
 
-        return {
-              checkState(x-1, y-1) + checkState(x, y-1) + checkState(x+1, y-1) 
-            + checkState(x-1, y)                        + checkState(x+1, y)
-            + checkState(x-1, y+1) + checkState(x, y+1) + checkState(x+1, y+1)
-        };
+        return (
+            checkState(x - 1, y - 1) + checkState(x, y - 1) + checkState(x + 1, y - 1)
+            + checkState(x - 1, y) + checkState(x + 1, y)
+            + checkState(x - 1, y + 1) + checkState(x, y + 1) + checkState(x + 1, y + 1));
     }
 
     constexpr bool isInBound(const Coord_type xPos, const Coord_type yPos) const
     {
-        return {
+        return (
             (xPos >= 0 && xPos < m_length)
-            && (yPos >= 0 && yPos < m_width)
-        };
+            && (yPos >= 0 && yPos < m_width));
     }
 
     constexpr bool checkState(const Coord_type xPos, const Coord_type yPos) const
     {
-        if (!isInBound(xPos, yPos))             // boundary condition: return dead state
-            return Cell::s_dead_state;
+        if (!isInBound(xPos, yPos)) {    // boundary condition: return dead state
+            // int effX{ xPos % m_length + 2 * (xPos < 0) - 1 };
+            // int effY{ yPos % m_width + 2 * (yPos < 0) - 1 };
+            // std::cout << m_length << '/' << m_width << ": ";
+            // std::cout << xPos << '/' << yPos << " -> ";
+            int effX{ (m_length + xPos % m_length) % m_length };    // following python behavior (surprisingly operator% behaves differently between python and cpp
+            int effY{ (m_width + yPos % m_width) % m_width };
+            // std::cout << effX << '/' << effY << '\n';
+
+            return (*this)(effX, effY).isLive();
+            // return Cell::s_dead_state;
+        }
 
         return (*this)(xPos, yPos).isLive();
     }
@@ -163,39 +229,80 @@ public:
     }
 
     const auto& data() const { return m_grid; }
-    auto& getCell(int col, int row) { return (*this)(col, row); }
-    const auto& getCell(int col, int row) const { return (*this)(col, row); }
+
+    // __return_cell_type can be anything as long as you provided the conversion function in pred.
+    // if __return_cell_type == bool, you can use data_flat_bool() instead
+    // if __return_cell_type == Cell, you can use data_flat_Cell() instead
+    template <typename __return_cell_type>
+    const auto data_flat(std::function<__return_cell_type(const Cell&)> pred) const
+    {
+        std::vector<__return_cell_type> vec;
+        for (std::size_t y{ 0 }; y < m_width; ++y) {
+            for (std::size_t x{ 0 }; x < m_length; ++x) {
+                vec.push_back(pred((*this)(x, y)));
+            }
+        };
+        return vec;
+    }
+
+    const auto data_flat_bool() const
+    {
+        std::vector<bool> vec;
+        for (std::size_t y{ 0 }; y < m_width; ++y) {
+            for (std::size_t x{ 0 }; x < m_length; ++x) {
+                vec.push_back(bool((*this)(x, y)));
+            }
+        }
+        return vec;
+    }
+
+    const auto data_flat_Cell() const
+    {
+        std::vector<bool> vec;
+        for (std::size_t y{ 0 }; y < m_width; ++y) {
+            for (std::size_t x{ 0 }; x < m_length; ++x) {
+                vec.push_back((*this)(x, y));
+            }
+        }
+        return vec;
+    }
+
+    auto&            getCell(int col, int row) { return (*this)(col, row); }
+    const auto&      getCell(int col, int row) const { return (*this)(col, row); }
     const Coord_type getWidth() const { return m_width; }
     const Coord_type getLength() const { return m_length; }
 
     // return length, width
     const std::pair<int, int> getDimension() const { return { m_length, m_width }; }
 
-    Cell& operator() (const Coord_type xPos, const Coord_type yPos)
+    constexpr Cell& operator()(const Coord_type xPos, const Coord_type yPos)
     {
-        if (!isInBound(xPos, yPos))
-            throw std::range_error{ "out of range" };
+        if (!isInBound(xPos, yPos)) {
+            throw std::out_of_range{ "out of range" };
+        }
 
         return m_grid[yPos][xPos];
     }
-    const Cell& operator() (const Coord_type xPos, const Coord_type yPos) const
+    constexpr const Cell& operator()(const Coord_type xPos, const Coord_type yPos) const
     {
-        if (!isInBound(xPos, yPos))
+        if (!isInBound(xPos, yPos)) {
             throw std::range_error{ "out of range" };
+        }
 
         return m_grid[yPos][xPos];
     }
 
     friend std::ostream& operator<<(std::ostream& out, const Grid& grid)
     {
-        for (auto& row : grid.m_grid)
-        {
-            for (auto& cell : row)
-            {
-                switch (cell.isLive())
-                {
-                case true:  std::cout << "█"; break;
-                case false: std::cout << " "; break;
+        for (auto& row : grid.m_grid) {
+            for (auto& cell : row) {
+                switch (cell.isLive()) {
+                case true:
+                    std::cout << "█";
+                    break;
+                case false:
+                    std::cout << " ";
+                    break;
                 }
             }
             std::cout << '\n';
@@ -203,6 +310,5 @@ public:
         return out;
     }
 };
-
 
 #endif
