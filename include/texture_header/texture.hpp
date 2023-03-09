15117eacf6a0e69659e10095eb9d5ee9203f184b
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-03-09 18:39:19 +0700

    Add new methods to Texture class and its corresponding members

--- include/texture_header/texture.hpp
+++ include/texture_header/texture.hpp
@@ -1,34 +1,50 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
-#include <glad/glad.h>  // include glad to get all the required OpenGL headers
+#include <glad/glad.h>    // include glad to get all the required OpenGL headers
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 
+#include <cstddef>
 #include <iostream>
 #include <limits>
-
+#include <unordered_map>
 
 class Texture
 {
+private:
     static inline unsigned int s_textureUnitCount{ 0 };
 
     int imageWidth{};
     int imageHeight{};
     int nrChannels{};
+
     unsigned char* imageData{};
 
+    GLenum magFilter{};
+    GLenum minFilter{};
+    GLenum wrapFilter{};
+
+    inline static const std::unordered_map<std::size_t, GLenum> numChannelsToGLenum{
+        { 1, GL_RED },
+        { 2, GL_RG },
+        { 3, GL_RGB },
+        { 4, GL_RGBA }
+    };
 
 public:
     static inline constexpr unsigned int maxUnitNum{ std::numeric_limits<unsigned int>::max() };
 
-    unsigned int textureUnitNum{ maxUnitNum };      // means no texture loaded
-    unsigned int textureID;
+    unsigned int textureUnitNum{ maxUnitNum };    // means no texture loaded
+    unsigned int textureID;                       // don't change this value outside of Texture class
 
     Texture(const Texture&) = default;
 
     // texture but basic material actually
-    Texture(const unsigned char red=0x0, const unsigned char green=0x0, const unsigned char blue=0x0)
+    Texture(
+        const unsigned char red   = 0x0,
+        const unsigned char green = 0x0,
+        const unsigned char blue  = 0x0)
     {
         textureUnitNum = s_textureUnitCount++;
 
@@ -40,7 +56,7 @@ public:
         imageData[1] = green;
         imageData[2] = blue;
 
-        imageWidth = 1;
+        imageWidth  = 1;
         imageHeight = 1;
 
         generateTexture();
@@ -50,30 +66,88 @@ public:
     }
 
     Texture(
+        unsigned char* colorData,
+        int            width,
+        int            height,
+        int            channels  = 3,
+        int            minFilter = GL_LINEAR,
+        int            magFilter = GL_NEAREST,
+        int            wrap      = GL_REPEAT)
+        : imageData{ colorData }
+        , imageWidth{ width }
+        , imageHeight{ height }
+        , nrChannels{ channels }
+    {
+        textureUnitNum = s_textureUnitCount++;
+
+        if (!imageData) {
+            std::cerr << "Failed to load texture: " << (void*)imageData << '\n';
+        } else {
+            std::cerr << "Successfully load texture from colorData (" << textureUnitNum << ")\n";
+            generateTexture(minFilter, magFilter, wrap);
+        }
+    }
+
+    Texture(
         const char* texFilePath,
-        int minFilter=GL_LINEAR,
-        int magFilter=GL_NEAREST,
-        int wrap=GL_REPEAT,
-        bool flipVertically=true
-    )
+        int         minFilter      = GL_LINEAR,
+        int         magFilter      = GL_NEAREST,
+        int         wrap           = GL_REPEAT,
+        bool        flipVertically = true)
     {
         textureUnitNum = s_textureUnitCount++;
 
         stbi_set_flip_vertically_on_load(flipVertically);
         imageData = stbi_load(texFilePath, &imageWidth, &imageHeight, &nrChannels, 0);
 
-        if(!imageData)
+        if (!imageData) {
             std::cerr << "Failed to load texture: " << texFilePath << '\n';
-        else
+        } else {
+            std::cerr << "Successfully load texture: " << texFilePath << " (" << textureUnitNum << ")\n";
             generateTexture(minFilter, magFilter, wrap);
-    
+        }
+
         stbi_image_free(imageData);
         imageData = nullptr;
     }
 
+    void updateTexture(void* data, int width, int height, int numChannels = 3, GLenum dataType = GL_UNSIGNED_BYTE)
+    {
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, numChannelsToGLenum[numChannels], width, height, 0, numChannelsToGLenum[numChannels], dataType, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    void updateMagFilter(GLenum magFilter)
+    {
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+    }
+
+    void updateMinFilter(GLenum minFilter)
+    {
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+    }
+
+    void updateWrapFilter(GLenum wrap)
+    {
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
+    }
+
+    void updateFilters(GLenum magFilter, GLenum minFilter, GLenum wrap)
+    {
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
+    }
+
 private:
-    // TODO: make a generateTexture() function that accepts parameters of texture wrap, texture min filter, and texture mag filter.
-    void generateTexture(int minFilter=GL_LINEAR, int magFilter=GL_NEAREST, int wrap=GL_REPEAT)
+    void generateTexture(int minFilter = GL_LINEAR, int magFilter = GL_NEAREST, int wrap = GL_REPEAT)
     {
         // generate texture
         glGenTextures(1, &textureID);
@@ -89,19 +163,9 @@ private:
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
 
         // now generate texture from image
-        int imageFormat{ GL_RGB };
-        if (nrChannels < 3)
-        {
-            std::cerr << "Image's color channel is less than 3 (no function can handle that for now)" << '\n';
-            return;
-        }
-        else if (nrChannels == 4)
-            imageFormat = GL_RGBA;
- 
-        glTexImage2D(GL_TEXTURE_2D, 0, imageFormat, imageWidth, imageHeight, 0, imageFormat, GL_UNSIGNED_BYTE, imageData);
+        glTexImage2D(GL_TEXTURE_2D, 0, numChannelsToGLenum[nrChannels], imageWidth, imageHeight, 0, numChannelsToGLenum[nrChannels], GL_UNSIGNED_BYTE, imageData);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
 };
-
 
 #endif
